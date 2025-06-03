9b6fbcfbc13f694b85c0ebabce93036b6c0bc24c
Wyattb11232006 <WYattb15@nycstudents.net>
2025-06-03 12:14:02 -0400

    Update tasks.js

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 // There is a bug in line 4 you need to fix it
-const taskModel = require("../model/taskModel");
-
+const taskModel = require("../models/taskModel");
+//It isn't going to work because it doesn't have an s
 //Write a comment describing the purpose of this route
 router.get("/", async (req, res) => {
   const tasks = await taskModel.getTasks();
@@ -12,8 +12,9 @@ router.get("/", async (req, res) => {
 // Write a comment describing the purpose of this route
 router.post("/", async (req, res) => {
   //there is a bug in line 15 you need to fix
-  const { name, description } = req.body;
+  const { title, description } = req.body;
   const task = await taskModel.addTask(title, description);
+  //It doesn't work because the tasks don't match up
   res.status(201).json(task);
 });
 
